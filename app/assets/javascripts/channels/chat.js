e570fea29c9929e30e111a7a08b826f5f6c9b964
thunderenlight <thunderenlight@gmail.com>
2018-06-08 14:04:00 -0500

    adding chat and view

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -1,13 +1,49 @@
 App.chat = App.cable.subscriptions.create("ChatChannel", {
-  connected: function() {
+  received: prepend_msg
     // Called when the subscription is ready for use on the server
-  },
+  })
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
+ addEventListener('turbolinks:load', function() {
+ 	join_chat_form.addEventListener( 'submit', join_chat)
+ 	message_form.addEventListener( 'submit', send_msg)
+ })
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+function join_chat(event) {
+	event.preventDefault()
+
+	if (!username.value)
+		return
+	enter_name_container.style.display = 'none'
+	messages_container.style.display = 'block'
+
+	const msg = `${username.value} joined the chat`
+	App.chat.send({ msg })
+
+	message.focus()
+}
+
+function send_msg(event) {
+	event.preventDefault()
+
+	const msg = `${username.value}: ${message.value}`
+	App.chat.send({ msg })
+
+	message.value = ''
+}
+
+function prepend_msg(data) {
+	const p = document.createElement('p')
+	add_time_stamp(p)
+	p.append(data.msg)
+	messages.prepend(p)
+}
+
+function add_time_stamp(p) {
+	const
+		time = new Date().toLocaleTimeString(),
+		pre = document.createElement('pre')
+
+	pre.append(time)
+	p.append(pre)
+}
+ 
